1231386a23464ec251cc5fa513eca325c2511f1b
maphick <maphick@yandex.ru>
2024-11-04 23:15:36 +0300

    Develop branch was created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.mariiadeveloper.unicornmessenger"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.mariiadeveloper.unicornmessenger"
